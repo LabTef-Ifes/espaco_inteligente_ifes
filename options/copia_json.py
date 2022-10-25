4cf33dd1dc0f991534de94d7840819323199b87d
Deivid Braian <deividbraian5@gmail.com>
2022-10-24 22:08:10 -0300

    Atualização do copia_json e mais summaries nos def

--- options/copia_json.py
+++ options/copia_json.py
@@ -1,19 +1,37 @@
 import shutil
 import json
+
+
 source_file = '0.json'
 with open(source_file) as f:
-	config = json.load(f)
-	print(config)
-	novo_fps = input("Insira o novo valor de fps:")
-	while not novo_fps.isnumeric():
-		print("Entrada inválida, insira um número inteiro válido")
-		novo_fps = input("Insira o novo valor de fps:")
-	novo_fps = int(novo_fps)
-	config['initial_config']['sampling']['frequency'] = novo_fps
+    config = json.load(f)
+    # print(config)
 
-with open(source_file,'w+') as f:
-	json.dump(config,f,indent = 2)
-for c in range(1,4):
-	shutil.copyfile(source_file,str(c)+'.json')
+fps = config['initial_config']['sampling']['frequency']
+novo_fps = input("Insira o novo valor de  ou enter para manter:") or fps
 
-	
+while True:
+    try:
+        novo_fps = float(novo_fps)
+    except:
+        print("Entrada inválida, insira um número inteiro válido")
+        novo_fps = input("Insira o novo valor de  ou enter para manter:") or fps
+
+    else:
+        break
+
+
+config['initial_config']['sampling']['frequency'] = novo_fps
+
+with open(source_file, 'w+') as f:
+    json.dump(config, f, indent=2)
+for c in range(1, 4):
+    shutil.copyfile(source_file, str(c)+'.json')
+
+
+# Atualiza o options.json na outra pasta
+options_path = '..\\dataset-creator\\options.json'
+
+with open(options_path) as f:
+    options = json.load(f)
+    print(options)
