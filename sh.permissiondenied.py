6531afd72eac7ae72c4a5c5707bfe1b4e5e8f3a0
Deivid Braian <deividbraian5@gmail.com>
2022-10-29 14:52:41 -0300

    atualização de docstring

--- sh.permissiondenied.py
+++ sh.permissiondenied.py
@@ -1,11 +1,11 @@
-'''Caso obtenha o erro
+'''Execute esse script caso obtenha o erro
+
 sh: 1: ./is-basic.sh: Permission denied
 sh: 1: ./is-cameras.sh: Permission denied
 sh: 1: ./is-skeletons-detector.sh: Permission denied
 sh: 1: ./is-frame-transformation.sh: Permission denied
 sh: 1: ./is-skeletons-grouper.sh: Permission denied
-
-Execute esse script'''
+'''
 
 import os
 os.system("chmod +x is-basic.sh")
