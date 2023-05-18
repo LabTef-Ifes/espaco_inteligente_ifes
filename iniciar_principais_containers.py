48d4905002c3854e3bb820f83dad891ad2d27a9c
Braian Smarzaro <deividbraian5@gmail.com>
2023-05-18 15:09:53 -0300

    atualiza a repo interna pra is-camera-py-deivid

--- iniciar_principais_containers.py
+++ iniciar_principais_containers.py
@@ -2,7 +2,7 @@
 import os
 import time
 os.system('sudo docker container stop $(sudo docker container ls -q)') # Para todos os containeres que estejam rodando antes.
-os.system('cd is-cameras-py/deploy/multi-camera && sudo docker compose up -d')
+os.system('cd is-cameras-py-deivid/deploy/multi-camera && sudo docker compose up -d')
 #os.system("./sh_files/is-basic.sh")
 #os.system("./sh_files/is-cameras.sh")
 time.sleep(5)
