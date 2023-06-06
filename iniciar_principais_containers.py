b8143d9c2ebbce936122408054b1eee2ff7c4134
Braian Smarzaro <deividbraian5@gmail.com>
2023-06-06 16:31:30 -0300

    atualiza o start dos dockers

--- iniciar_principais_containers.py
+++ iniciar_principais_containers.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 import os
 import time
-os.system('sudo python sh_permission_denied.py')
 os.system('sudo docker container stop $(sudo docker container ls -q)') # Para todos os containeres que estejam rodando antes.
-try:
-    os.system('cd is-cameras-py-labtef/deploy/multi-camera && sudo docker compose up -d')
-except:
-    print("Não foi possível iniciar o docker compose up")
+os.system('sudo python sh_permission_denied.py')
+#os.system('cd is-cameras-py-labtef/deploy/multi-camera && sudo docker compose up -d')
 #os.system("./sh_files/is-basic.sh")
 #os.system("./sh_files/is-cameras.sh")
-time.sleep(5)
-os.system("./sh_files/is-skeletons-detector.sh")
+#time.sleep(5)
+os.system("./sh_files/is-rabbitmq.sh")
+os.system("./sh_files/is-zipkin.sh")
+os.system("./sh_files/is-mjpeg.sh")
+#os.system("./sh_files/is-skeletons-detector.sh")
 os.system("./sh_files/is-frame-transformation.sh")
 os.system("./sh_files/is-skeletons-grouper.sh")
--- sh_permission_denied.py
+++ sh_permission_denied.py
@@ -11,6 +11,7 @@ sh: 1: ./sh_files/is-skeletons-grouper.sh: Permission denied
 import os
 os.system("chmod +x ./sh_files/is-rabbitmq.sh")
 os.system("chmod +x ./sh_files/is-zipkin.sh")
+os.system("chmod +x ./sh_files/is-mjpeg.sh")
 os.system("chmod +x ./sh_files/is-cameras.sh")
 os.system("chmod +x ./sh_files/is-skeletons-detector.sh")
 os.system("chmod +x ./sh_files/is-frame-transformation.sh")
