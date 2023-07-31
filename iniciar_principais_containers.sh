c2b30e8b5f4794a3fe1deba9c8e51113974c6b61
Deivid Braian <deividbraian5@gmail.com>
2023-07-31 09:01:24 -0300

    transforma os scripts em bash e cria um teste

---
+++ iniciar_principais_containers.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Stop all running containers
+sudo docker container stop $(sudo docker container ls -q)
+
+# Execute sh_permission_denied.py using Python
+sudo python sh_permission_denied.py
+
+# Change directory and start Docker Compose in detached mode
+cd is-cameras-py-labtef/deploy/multi-camera && sudo docker compose up -d
+
+# Wait for 7 seconds
+sleep 7
+
+# Uncomment the lines below if you want to run the following scripts:
+#./sh_files/is-cameras.sh
+#./sh_files/is-rabbitmq.sh
+#./sh_files/is-zipkin.sh
+#./sh_files/is-mjpeg.sh
+
+# Run the specified scripts
+./sh_files/is-skeletons-detector.sh
+./sh_files/is-frame-transformation.sh
+./sh_files/is-skeletons-grouper.sh
