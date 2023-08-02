33f24b95213c9726e1c51d274e3f65e5c4840053
Deivid_PC20 <deividbraian@gmail.com>
2023-08-02 10:07:55 -0300

    rebase

---
+++ iniciar_sem_camera.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Stop and remove all running containers
+sudo docker rm -f $(sudo docker container ls -q)
+
+# Execute sh_permission_denied.py using Python
+sudo python sh_permission_denied.py
+
+# Wait for 1 second between each command
+sleep 1
+
+# Run the specified scripts one by one, waiting 1 second between each
+./sh_files/is-rabbitmq.sh
+sleep 1
+
+./sh_files/is-zipkin.sh
+sleep 1
+
+./sh_files/is-mjpeg.sh
+sleep 1
+
+./sh_files/is-skeletons-detector.sh
+sleep 1
+
+./sh_files/is-frame-transformation.sh
+sleep 1
+
+./sh_files/is-skeletons-grouper.sh
