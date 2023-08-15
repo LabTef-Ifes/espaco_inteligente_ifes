9d75736b7ed9eb6450a53d0aeecdaa7c7ff7d9f0
Deivid_PC20 <deividbraian@gmail.com>
2023-08-15 15:59:44 -0300

    mais capturas

--- dataset-creator/videos/delete_files.py
+++ dataset-creator/videos/delete_files.py
@@ -3,8 +3,7 @@ import glob
 
 # Get the current directory
 current_dir = os.getcwd()
-path = os.path.join(current_dir, "p002g01")
+path = os.path.join(current_dir, "p001g01")
 #Apaga as imagens que terminam com valores menores que 80
 for filename in glob.glob(os.path.join(path, '*.jpeg')):
-    if int(filename[-7:-5]) > 50:
-        os.remove(filename)
+    os.remove(filename)
