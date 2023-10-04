0d85f687cd934f00017266f119657f1c395e07f9
Deivid_PC20 <deividbraian@gmail.com>
2023-10-04 14:45:10 -0300

    deleta copias

--- sh_files/is-skeletons-grouper.sh
+++ sh_files/is-skeletons-grouper.sh
@@ -8,7 +8,7 @@ docker container run -d --rm \
     #./service.bin /options.json 
     #./rpc.bin /options.json 
 
- docker container run -d --rm \
+docker container run -d --rm \
  	-v $PWD/is-skeletons-grouper/options.json:/opt/is/options.json \
  	--memory=2g \
  	--cpus=1 \
@@ -18,23 +18,5 @@ docker container run -d --rm \
      ./service.bin /options.json 
      #./rpc.bin /options.json 
 
- docker container run -d --rm \
- 	-v $PWD/is-skeletons-grouper/options.json:/opt/is/options.json \
- 	--memory=2g \
- 	--cpus=1 \
- 	--network=host \
- 	--name=grouper3 \
- 	mendonca/is-skeletons-grouper:0.0.4-debug  ./rpc.bin /options.json
-     ./service.bin /options.json 
-     #./rpc.bin /options.json 
 
- docker container run -d --rm \
- 	-v $PWD/is-skeletons-grouper/options.json:/opt/is/options.json \
- 	--memory=2g \
- 	--cpus=1 \
- 	--network=host \
- 	--name=grouper4 \
- 	mendonca/is-skeletons-grouper:0.0.4-debug  ./rpc.bin /options.json
-    #./service.bin /options.json 
-    #./rpc.bin /options.json 
     
