e6949696aca7f75a6f8b6f707e77ef6e888bf5db
Braian <deividbraian5@gmail.com>
2023-11-08 15:39:43 -0300

    rename mjpeg contaner

--- sh_files/is-mjpeg.sh
+++ sh_files/is-mjpeg.sh
@@ -1,6 +1,6 @@
 echo 'viros/is-mjpeg-server:1'
 
-docker run -d --name is-mjpeg-server5 \
+docker run -d --name is-mjpeg-server \
     -p 3000:3000 \
     -e IS_URI=amqp://guest:guest@rabbitmq:5672 \
     --restart=on-failure:5 \
