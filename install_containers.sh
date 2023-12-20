f994c79bee92c85e4eba1bf338cd89f2f19efca3
Braian Pc Guarapari <deividbraian5@gmail.com>
2023-12-20 16:43:33 -0300

    insert sudo

--- install_containers.sh
+++ install_containers.sh
@@ -1,8 +1,8 @@
 # Download all container images
-docker pull viros/is-cameras:1.5
-docker pull openzipkin/zipkin
-docker pull rabbitmq:3.7.6-management
-docker pull mendonca/is-skeletons-grouper:0.0.4-debug
-docker pull labviros/is-skeletons-detector:0.0.2-openpose
-docker pull labviros/is-mjpeg-server:0.0.1
-docker pull labviros/is-frame-transformation:0.0.4
+sudo docker pull viros/is-cameras:1.5
+sudo docker pull openzipkin/zipkin
+sudo docker pull rabbitmq:3.7.6-management
+sudo docker pull mendonca/is-skeletons-grouper:0.0.4-debug
+sudo docker pull labviros/is-skeletons-detector:0.0.2-openpose
+sudo docker pull labviros/is-mjpeg-server:0.0.1
+sudo docker pull labviros/is-frame-transformation:0.0.4
