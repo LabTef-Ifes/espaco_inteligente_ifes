ed8e0bb4cb2efacf185c466210d7794670ec68da
Deivid Braian <deividbraian5@gmail.com>
2024-02-10 09:13:35 -0300

    delete sudo from commands and add network

--- venv.sh
+++ venv.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --name venv -v /var/run/docker.sock:/var/run/docker.sock -v $PWD:/app smarzarocom/env_is 
+docker run -it --rm --name venv --network host -v /var/run/docker.sock:/var/run/docker.sock -v ${PWD}:/app smarzarocom/env_is
